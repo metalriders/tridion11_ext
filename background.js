dc72721dca333afdef58e7b5d9bde4ba8f546f7d
metalriders <daniboy307@outlook.com>
2017-03-17 12:30:44 -0600

    Refactor background

--- background.js
+++ background.js
@@ -1,46 +1,29 @@
 console.log("Loading Tridion Extension");
 
-var callback;
-var filters =
-{
-	url: 
-	[
-		//{hostSuffix:"epocms.www8.hp.com"}
-		{pathContains:"ListFilters/SearchListBar.aspx"}
-	]
-};
-
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-// 	chrome.tabs.executeScript(tab.tabId, {"code":'console.log("hello world from the other side")'}, function (){
-// 		console.log("Executed Script 2");
-// 	});
-// }); 
-
-// UTILS
-function msg_log(str)
-{
-	console.info(str);
-}
 
 
 /*
 	Listen to load of Tridion Dashboard and inject the extension
 */
-callback = function(tab) {
-	var details;
-	console.debug('Loading Tridion Extension');
+chrome.webNavigation.onCompleted.addListener(
+	function()
+	{
+		var details = {runAt: "document_end"};
+		console.debug('Loading Tridion Extension');
 
-	details = {file:"jquery_3.min.js", runAt: "document_end"};
-	chrome.tabs.executeScript( tab.tabId, details, msg_log("loaded jquery_3"));
+		details["file"] = "jquery_3.min.js";
+		chrome.tabs.executeScript( tab.tabId, details, msg_log("loaded jquery_3"));
 
-	details = {file:"popup.js", runAt:"document_end"};
-	chrome.tabs.executeScript( tab.tabId, details, msg_log("loaded tridion_ext"));
+		details["file"] = "popup.js";
+		chrome.tabs.executeScript( tab.tabId, details, msg_log("loaded tridion_ext"));
 
-	chrome.tabs.sendMessage(tab.tabId, {"action": "dashboard_load"}, function (){
-		console.debug("Finished load of tridion!");
-	});
-}
-chrome.webNavigation.onCompleted.addListener(callback, filters);
+		chrome.tabs.sendMessage(tab.tabId, {"action": "dashboard_load"}, function (){
+			console.debug("Finished load of tridion!");
+		});
+	}
+	// URLfilter
+	, {url:[{pathContains:"ListFilters/SearchListBar.aspx"}]}		
+);
 
 /*
 	Click listener for extension button
@@ -52,4 +35,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     code: 'document.body.style.backgroundColor="red"'
   });
 });
-callback = null;
+
+// UTILS
+function msg_log(str)
+{
+	console.info(str);
+}
