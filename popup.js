034f1b27cbbdadf659dac4223415eb64dc0846a9
metalriders <daniboy307@outlook.com>
2017-03-18 21:29:57 -0600

    Added native message handle

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@
 // Reserved for comms
 chrome.runtime.onMessage.addListener(function(msg)
 {
-  console.info("Yo I got a message!");
+  alert("Popup - Yo I got a message!");
   console.debug(msg);
 });
 
@@ -12,3 +12,12 @@ var scr = document.createElement('script');
 scr.type="text/javascript";
 scr.src= chrome.extension.getURL('tridion_ext.js');;
 document.head.appendChild(scr);
+
+window.addEventListener("message", function(event) {
+  // We only accept messages from ourselves
+  if (event.source != window)
+    return;
+
+  if (event.data.action && (event.data.action == "open_item"))    chrome.runtime.sendMessage(event.data);
+
+}, false);
--- tridion_ext.js
+++ tridion_ext.js
@@ -1,25 +1,43 @@
+// Author: Daniel G (oscar-daniel.gonzalez@hp.com)
+
 new class Tridion_Ext
 {  
   constructor()
   {
     this._v = 1.0;
-    this.dashboard = document.querySelector("#DashboardContextMenu");
+    this.ctx_menu = document.querySelector("#DashboardContextMenu");
+    this.dashboard_tree = document.querySelector("#DashboardTree iframe");
     this.dashboard_list = document.querySelector("#FilteredDashboardList");
-    this.list_items = undefined;
-    this.items = undefined;
+    this.publications_refs = [];
+    this.list_lvls = [];
+    this.list_items = [];
+    this.items = [];
+    this.lvls = [];
 
     this.init();
   }
 
-  observer_wrapper(target, config, callback)
+  init()
   {
-    var observer = new MutationObserver(callback);
-    observer.observe(target, config);
-    return function removeElementChange() {
-        observer.disconnect();
-    };
+    console.debug("Tridion Extension v"+ this._v +", made by Daniel G [oscar-daniel.gonzalez@hp.com]");
+    var _self = this;
+    
+      this.publications_refs = this.dashboard_tree.contentDocument.querySelectorAll("div.rootNode.populated div.children div.node");
+      
+      this.publications_refs
+        .forEach(function(publication) 
+        {
+          var id = publication.id.split(':')[1].split('-')[1];
+          var lvl = publication.querySelector(".header .title").title;
+          lvl = lvl.slice(0, lvl.indexOf(" ("));  // lvl   remove what is between parenthesis
+          _self.lvls[id] = unescape(lvl);
+        });
+    
+    this.add_actions();
+    this.add_observers();
   }
   
+/* Actions*/
   enable_pro_publishing()
   {
     var _self = this;
@@ -44,10 +62,10 @@ new class Tridion_Ext
 
     this.pro_publishing.onclick = function()
     {
-      console.log("Do magic");
+      console.log("Publishing items");
     }    
 
-    this.dashboard.appendChild(this.pro_publishing);
+    this.ctx_menu.appendChild(this.pro_publishing);
   }
 
   enable_open_mult_items()
@@ -75,29 +93,68 @@ new class Tridion_Ext
 
     this.mult_open.onclick = function()
     {
-      var sel = _self.items.querySelectorAll("tr.selected");     // check dependency
-      // Tridion does not support double click trigger on folders
-      console.log("Do magic");
+      if(_self.items.length == 0) return;
+      var sel_items = _self.items.querySelectorAll("tr.selected");
+      
+      sel_items
+        .forEach(function(item) 
+        {
+          if(item.type == 16)  // component
+          {
+            console.log("Opening tcm"+ item.tcm_id + " from " + _self.lvls[item.lvl]);
+            var url_base = "https://epocms.www8.hp.com/WebUI/item.aspx?tcm=16#id=tcm:"+item.lvl+"-"+item.tcm_id;
+            window.postMessage({action: "open_item", url: url_base}, "*");    // send a message to background to handle request
+          }
+          else        // everything else
+          {
+            console.log("Type not supported");
+          }
+        });
+      // Example of url for items https://epocms.www8.hp.com/WebUI/item.aspx?tcm=16#id=tcm:50-1719412
+      // Tridion does not support double click trigger on folder      
     }
 
-    this.dashboard.appendChild(this.mult_open);
+    this.ctx_menu.appendChild(this.mult_open);
   }
 
+/* Storage */
   get_localStorage()
   {
   }
 
-  init()
+  update_frame_items()
   {
-    var _self = this;
-    console.debug("Tridion Extension v"+ this._v +", made by Daniel G [oscar-daniel.gonzalez@hp.com]");
-    
-    // Separate new actions
+    this.frame_items = document.querySelector("#FilteredItemsList_frame_details");
+    this.items = this.frame_items.contentWindow.document.querySelector("table");
+    this.process_items();
+  }
+
+/* Wrappers */
+  observer_wr(target, config, callback)
+  {
+    var observer = new MutationObserver(callback);
+    observer.observe(target, config);
+    return function removeElementChange() {
+        observer.disconnect();
+    };
+  }
+
+/* Main Functions */
+  add_actions()
+  {
     var sep = document.createElement("li");
     sep.className = "separator";
-    this.dashboard.appendChild(sep);
+    this.ctx_menu.appendChild(sep);
 
-    this.observer_wrapper(
+    this.enable_pro_publishing();
+    this.enable_open_mult_items();
+  }
+
+  add_observers()
+  {
+    var _self = this;
+    
+    this.observer_wr(
       this.dashboard_list, { subtree: true, childList: true},function()
       {
         _self.list_items = document.querySelector("#FilteredItemsList_frame_details");
@@ -111,13 +168,36 @@ new class Tridion_Ext
           this.disconnect();
         }
       });
-    this.enable_pro_publishing();
-    this.enable_open_mult_items();
   }
 
-  update_frame_items()
+  process_items()
   {
-    this.frame_items = document.querySelector("#FilteredItemsList_frame_details");
-    this.items = this.frame_items.contentWindow.document.querySelector("table");
+    var _self = this;
+    this.items.querySelectorAll("tr.item")
+      .forEach(function(row) 
+      {
+        var cols = row.querySelectorAll("td");
+        var item_lvl = cols[3].querySelector("div");
+        var txt_lvl = _self.unicode_to_ascii(item_lvl.innerText);
+        row.tcm_id = row.id.split(':')[1].split('-')[1];
+        row.type = cols[2].getAttribute("value");
+        row.lvl = _self.get_id_by_lvl(txt_lvl);  
+      });
   }
+
+/* UTILS */  
+  get_id_by_lvl(lvl)
+  {
+    return this.lvls.indexOf(lvl);
+  }
+
+  unicode_to_ascii(str)
+  {
+    var out = "";
+    for (var i = 0; i < str.length; i++) {
+        out += String.fromCharCode(str.charCodeAt(i) % 128);
+    }
+    return out;
+  }
+  
 };
