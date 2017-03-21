deeeaf00a76122e54f1dbb64e64ae34a2c1cb93c
metalriders <daniboy307@outlook.com>
2017-03-21 12:48:03 -0600

    Commented unwanted action

--- src/background.js
+++ src/background.js
@@ -23,9 +23,9 @@ chrome.webNavigation.onCompleted.addListener(
 chrome.browserAction.onClicked.addListener(function(tab) {
 	// No tabs or host permissions needed!
 	console.log('browserAction');
-	chrome.tabs.executeScript({
-		code: 'document.body.style.backgroundColor="red"'
-	});
+	// chrome.tabs.executeScript({
+	// 	code: 'document.body.style.backgroundColor="red"'
+	// });
 });
 
 /*
