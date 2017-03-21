bc2ef1e29d9f9c20303b1c05da1450f91abe9211
metalriders <daniboy307@outlook.com>
2017-03-21 12:44:14 -0600

    Initial options page

---
+++ src/options.js
@@ -0,0 +1,24 @@
+// Saves options to chrome.storage
+function save_options() {
+  chrome.storage.sync.set({
+    // values to save
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    // values to load
+  }, function(items) {
+    // TODO
+  });
+}
