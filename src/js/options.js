de0e01fd5fc0f6e3acda653e6f43e1942e3a6ff2
metalriders <daniboy307@outlook.com>
2017-03-25 03:30:13 -0600

    Template for options

--- src/js/options.js
+++ src/js/options.js
@@ -22,3 +22,50 @@ function restore_options() {
     // TODO
   });
 }
+
+class OptionLevel extends HTMLElement{
+  constructor(level){
+    super();
+    
+    var chkbx = document.createElement("input");
+    var span = document.createElement("span");
+    this.id = level.id;
+    chkbx.type = "checkbox";
+    chkbx.id = level.id;
+    span.textContent = level.name;
+    this.append(chkbx);
+    this.append(span);
+    return this;
+  }
+}
+customElements.define('option-level', OptionLevel);
+
+class OptionLevels extends HTMLElement{
+
+  addOption(level){
+    this.append(new OptionLevel(level));
+  }
+  
+  constructor(levels){
+    super();
+    levels.forEach(this.addOption, this);
+    return this;
+  }
+}
+customElements.define('option-levels', OptionLevels);
+
+class OptionsSection extends HTMLElement{
+  constructor(){
+    super();
+    if(this.getAttribute("custom")){
+      this.custom = true;
+    }
+    if(this.hasAttribute("name")){
+      var title = document.createElement("h2");
+      title.textContent = this.getAttribute("name");
+      this.append(title);
+    }
+    this.append(new OptionLevels([{name:"H", id:12},{name:"H", id:12},{name:"H", id:12},{name:"H", id:12},{name:"H", id:12},{name:"H", id:12},{name:"H", id:12},{name:"H", id:12},{name:"H", id:12}]));
+  }
+}
+customElements.define('option-section', OptionsSection);
