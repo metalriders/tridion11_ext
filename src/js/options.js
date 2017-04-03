6207d6d457f62fae5232bf8e33029ada4f26f060
metalriders <daniboy307@outlook.com>
2017-04-03 15:54:24 -0500

    save and load custom batches

--- src/js/options.js
+++ src/js/options.js
@@ -1,17 +1,3 @@
-// Saves options to chrome.storage
-function save_options() {
-  console.debug('saving options');
-  chrome.storage.sync.set({
-    // values to save
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
-}
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
@@ -25,7 +11,7 @@ function restore_options() {
 }
 
 class OptionLevel extends HTMLElement{
-  constructor(level){
+  constructor(level, checked=false){
     super();
     
     var chkbx = document.createElement("input");
@@ -33,6 +19,7 @@ class OptionLevel extends HTMLElement{
     chkbx.type = "checkbox";
     chkbx.id = level.id;
     chkbx.value = level.name;
+    chkbx.checked = checked;
     span.textContent = level.name;
     this.append(chkbx);
     this.append(span);
@@ -57,7 +44,7 @@ class OptionLevels extends HTMLElement{
 customElements.define('option-levels', OptionLevels);
 
 class OptionsSection extends HTMLElement{
-  constructor(name, custom){
+  constructor(name, custom, id){
     super();
     
     var title = document.createElement("h2");
@@ -65,6 +52,7 @@ class OptionsSection extends HTMLElement{
     this.append(title);  
 
     if(custom) {
+      this.id = id? id : new Date().valueOf();
       this.custom = true;
       var selected_levels = {};
       document.querySelectorAll("#main-section input:checked").forEach(function(input){
@@ -85,6 +73,46 @@ customElements.define('option-section', OptionsSection);
 
 (function(){
 
+  var ls_main_levels;
+  var ls_main_conf;
+  var ls_cust_batches;
+
+  // Saves options to chrome.storage
+  function save_options() {
+    console.debug('saving options');
+    
+    var options = {
+      "main_batch":{
+        "levels" : ls_main_levels, 
+        "conf":{}
+      },
+      "cust_batches": []
+    };
+
+    // Get main_batch changes
+    document.querySelectorAll("#main-section input:checked").forEach(function(item){options.main_batch.conf[item.value] = true;});
+
+    // Get custom batches
+    document.querySelectorAll("#custom-sections option-section").forEach(function(section){
+      var section_conf = {
+        "id": section.id,
+        "name": section.querySelector("h2").textContent,
+        "conf": []
+      };
+      section.querySelectorAll("input:checked").forEach(function(item){section_conf.conf.push(item.id);});
+
+      options.cust_batches.push(section_conf);
+    });
+
+    chrome.storage.local.set(options, function() {
+      // Update status to let user know options were saved.
+      var status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
+    });
+  }
   var customs = document.querySelector("#custom-sections");
   // Load personal settings
   chrome.storage.local.get(null, function(conf){
@@ -96,11 +124,17 @@ customElements.define('option-section', OptionsSection);
     }
     
     console.log("loading levels");
-    var all_lvl_container = document.querySelector("option-section");
-    var levels = new OptionLevels(conf.main_batch.levels);
+    ls_main_levels = conf.main_batch.levels;
+    ls_main_conf = conf.main_batch.conf;
+    ls_cust_batches = conf.cust_batches;
     
-    // input change listeners
+    // Load Main batch
+    var all_lvl_container = document.querySelector("option-section");
+    var levels = new OptionLevels(ls_main_levels);
     levels.querySelectorAll("input").forEach(function(input) {
+      input.checked = ls_main_conf[input.value];
+
+      // main level change listeners
       input.addEventListener("change", function () {
         var id =input.id;
         
@@ -122,8 +156,24 @@ customElements.define('option-section', OptionsSection);
           });
       })
     },this);
-
     all_lvl_container.appendChild(levels);
+
+    if(ls_cust_batches == undefined){
+      console.error("No custom batch found, create one first"); 
+      return; 
+    }
+    // Load custom batches
+    var cust_container = document.querySelector("#custom-sections");
+    ls_cust_batches.forEach(function(section){
+      var cust_section = new OptionsSection(section.name, true, section.id);
+      
+      // turn on selected items
+      section.conf.forEach(function(level){
+        cust_section.querySelector('[id="'+level+'"]').checked = true;
+      });
+
+      cust_container.appendChild(cust_section);
+    })
   });
 
   document.querySelector(".add_custom").addEventListener("click", function(){
