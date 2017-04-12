3da44f568f076f56cadc0ee18ee81f5a22d50b6a
metalriders <daniboy307@outlook.com>
2017-04-12 16:03:14 -0500

    inject UI html via javascript

---
+++ src/js/sup_publishing.js
@@ -0,0 +1,6 @@
+var html = '<div class="sp_main" draggable="true">  <div>    <table>      <thead></thead>      <tbody></tbody>    </table>  </div>  <div>    <div class="sp_batch_sel">      <label>Select batch for publishing</label>      <select>        <option>EN locales</option>      </select>    </div>    <input id="td11_unpublish_all" type="button" value="Unpublish">    <input id="td11_publish_all" type="button" value="Publish All">    <input id="td11_clear_all" type="button" value="Clear All">  </div></div>';
+var container = document.createElement("div");
+container.innerHTML = html;
+console.log(document);
+
+document.body.appendChild(container)
