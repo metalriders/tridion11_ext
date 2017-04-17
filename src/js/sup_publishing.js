405c1ac07495ad631adaaf10960fdf92dead5208
metalriders <daniboy307@outlook.com>
2017-04-17 11:52:45 -0500

    Removed placeholder option

--- src/js/sup_publishing.js
+++ src/js/sup_publishing.js
@@ -1,4 +1,4 @@
-var html = '<div class="sp_main" draggable="true">  <div>    <table>      <thead></thead>      <tbody></tbody>    </table>  </div>  <div>    <div class="sp_batch_sel">      <label>Select batch for publishing</label>      <select>        <option>EN locales</option>      </select>    </div>    <input id="td11_unpublish_all" type="button" value="Unpublish">    <input id="td11_publish_all" type="button" value="Publish All">    <input id="td11_clear_all" type="button" value="Clear All">  </div></div>';
+var html = '<div class="sp_main" draggable="true">  <div>    <table>      <thead></thead>      <tbody></tbody>    </table>  </div>  <div>    <div class="sp_batch_sel">      <label>Select batch for publishing</label><select></select></div><input id="td11_unpublish_all" type="button" value="Unpublish">    <input id="td11_publish_all" type="button" value="Publish All">    <input id="td11_clear_all" type="button" value="Clear All">  </div></div>';
 var container = document.createElement("div");
 container.innerHTML = html;
 console.log(document);
