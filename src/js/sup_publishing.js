e2870cfdfad95a6f969536b8dc01952f8d4973a2
metalriders <daniboy307@outlook.com>
2017-04-17 16:43:13 -0500

    Updated UI

--- src/js/sup_publishing.js
+++ src/js/sup_publishing.js
@@ -1,6 +1,4 @@
-var html = '<div class="sp_main" draggable="true">  <div>    <table>      <thead></thead>      <tbody></tbody>    </table>  </div>  <div>    <div class="sp_batch_sel">      <label>Select batch for publishing</label><select></select></div><input id="td11_unpublish_all" type="button" value="Unpublish">    <input id="td11_publish_all" type="button" value="Publish All">    <input id="td11_clear_all" type="button" value="Clear All">  </div></div>';
+var html = '<div class="sp_main"><table cellpadding="0" cellspacing="0" border="0" style=""><thead class="tbl-header"><tr><th>name</th><th>type</th><th>Date</th></tr></thead><tbody class="tbl-content"></tbody></table><div><div class="sp_batch_sel"><label>Select batch for publishing</label><span class="custom-dropdown"><select></select></span></div><input id="td11_unpublish_all" type="button" value="Unpublish"><input id="td11_publish_all" type="button" value="Publish All"><input id="td11_clear_all" type="button" value="Clear All"></div></div>';
 var container = document.createElement("div");
 container.innerHTML = html;
-console.log(document);
-
 document.body.appendChild(container)
